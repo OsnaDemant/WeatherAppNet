ea100cb1229ab71f73aeb35ad3e569848b44085b
Oskar Nowak <osna@demant.com>
2022-06-20 14:41:57 +0200

    Create WPF Aplication.

---
+++ WPFWeatherAPI/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WPFWeatherAPI;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    private string _cityName = string.Empty;
+    public string CityName {
+        get => _cityName;
+        set {
+            _cityName = value;
+            OnPropertyChanged(nameof(CityName));
+        }
+    }
+
+    public MyCommand GetWeatherCommand { get; set; }
+
+    public MainWindowViewModel()
+    {
+        var action = () =>
+        {
+            MessageBox.Show($"Wybrano: {CityName}");
+        };
+        GetWeatherCommand = new MyCommand(action);
+    }
+
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(string? propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ WPFWeatherAPI/MyCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFWeatherAPI
+{
+    public class MyCommand : ICommand
+    {
+        private readonly Action execute;
+
+        public MyCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            execute?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
+
