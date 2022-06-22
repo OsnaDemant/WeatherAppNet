86c3cce1596798fa69b5c4f142c4ca8a26f6aee2
Oskar Nowak <osna@demant.com>
2022-06-22 15:22:07 +0200

    Add Refresh CheckBox

--- WPFWeatherAPI/MainWindowViewModel.cs
+++ WPFWeatherAPI/MainWindowViewModel.cs
@@ -6,33 +6,46 @@ namespace WPFWeatherAPI;
 public class MainWindowViewModel : INotifyPropertyChanged
 {
     private string _cityName = string.Empty;
-    public string CityName {
+    public string CityName
+    {
         get => _cityName;
-        set {
+        set
+        {
             _cityName = value;
             GetWeatherCommand?.RaiseCanExecuteChanged();
             OnPropertyChanged(nameof(CityName));
         }
     }
+    private bool refreshCheckBoxIsChecked = false;
+    public bool RefreshCheckBoxIsChecked
+    {
+        get => refreshCheckBoxIsChecked;
+        set
+        {
+            refreshCheckBoxIsChecked = value;
+            OnPropertyChanged(nameof(refreshCheckBoxIsChecked));
+        }
+    }
+
 
     public MyCommand GetWeatherCommand { get; set; }
-    public My
 
     public MainWindowViewModel()
     {
         var action = () =>
         {
-            MessageBox.Show($"Wybrano: {CityName}");
+            MessageBox.Show($"Wybrano: {CityName}, refresh: {refreshCheckBoxIsChecked}");
         };
         var func = () =>
         {
-        
+
             if (CityName != string.Empty) { return true; }
             return false;
-            
+
         };
-        
-        GetWeatherCommand = new MyCommand(action,func);
+
+        GetWeatherCommand = new MyCommand(action, func);
+
     }
 
 
